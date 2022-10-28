e803aed091f6242bb6e2105c56046ce745901521
SheilaNS <shei.nsantos@gmail.com>
2022-10-28 14:52:38 -0300

    Ajeitando as fontes da home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,17 +7,17 @@ export default function Home() {
 			<div className={ home.bg }>
 				<div className={ home.content }>
 					<h1> Sheila Nakashima dos Santos</h1>
-					<p>
+					<p className={ home.text }>
 							Hi there! This is my portfolio. Here you'll be able to
 							know more about me, my skills and the projects that I
 							have developed until now.
 					</p>
-					<p>Hope you enjoy!</p>
+					<p className={ home.text }>Hope you enjoy!</p>
 				</div>
 			</div>
 			<div className={ home.body }>
 				<h2>Behind the scene of this portfolio</h2>
-				<p>Developed with React.js, responsive Bootstrap and CSS Modules.</p>
+				<p className={ home.text }>Developed with React.js, responsive Bootstrap and CSS Modules.</p>
 			</div>
 		</main>
 	);
