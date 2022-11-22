4e6df8ed8af9355f54b1f396b7e326ac11a45077
SheilaNS <shei.nsantos@gmail.com>
2022-11-22 19:12:52 -0300

    feat: add desing-system-project

---
+++ src/data/projectsRocket.js
@@ -0,0 +1,18 @@
+import desing from '../assets/images/projects/desing.png';
+import github from '../assets/icons/github.svg';
+import web from '../assets/icons/web.svg';
+
+
+const contacts = [
+  {
+    name: "Desing System",
+    image: desing,
+    description: "Going from Figma to React and building one desing system. Ignite Lab Project.",
+    gitHub: "https://github.com/SheilaNS/ignite-lab-desing-system",
+    gitIcon: github,
+    web: "https://ignite-lab-desing-system-sepia.vercel.app/",
+    webIcon: web,
+  },
+]
+
+export default contacts;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,6 +2,7 @@ import ProjectCard from "../components/ProjectCard";
 import Title from "../components/Title";
 import fundamentos from "../data/projectsFund";
 import front from "../data/projectsFront";
+import rocket from "../data/projectsRocket";
 import projects from "../assets/css/Projects.module.css";
 
 export default function Projects() {
@@ -27,6 +28,21 @@ export default function Projects() {
         <h3>Trybe: Front-End</h3>
         <div className={projects.projects}>
           {front.map((elem, index) => (
+            <ProjectCard
+              key={index}
+              name={elem.name}
+              desc={elem.description}
+              image={elem.image}
+              git={elem.gitHub}
+              gitIcon={elem.gitIcon}
+              web={elem.web}
+              webIcon={elem.webIcon}
+            />
+          ))}
+        </div>
+        <h3>RocketSeat</h3>
+        <div className={projects.projects}>
+          {rocket.map((elem, index) => (
             <ProjectCard
               key={index}
               name={elem.name}
